97ccf4c537a8c8f477df058293d13b12cafd4700
smani004c <sivaram_mani2@cable.comcast.com>
2016-04-12 22:24:13 -0600

    QR res print

--- RPiQR.py
+++ RPiQR.py
@@ -26,12 +26,13 @@ time.sleep(0.2)
 font = cv2.FONT_HERSHEY_SIMPLEX
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array # CV Image in BGR format
-    print "img convert"
+    #print "img convert"
     qrimage = PILImage.fromarray(image)
-    print "img zbar check"
+    #print "img zbar check"
     codes = zbarlight.scan_codes('qrcode', qrimage)
-    print "img shape check"
-    print(image.shape)
+    print codes
+    #print "img shape check"
+    #print(image.shape)
     #code = DecodeQRImage(image)
     #cv2.putText(image,codes, (100, 200), font, 2, (0, 0, 255), 2)
     cv2.imshow("Pi Camera Feed", image)
